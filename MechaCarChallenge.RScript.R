3579eea9c08cec6bf31f96fc605b8e697546f247
Vivek Gurumoorthy <108832056+vivek-gurumoorthy@users.noreply.github.com>
2022-11-05 21:09:12 -0400

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -5,3 +5,12 @@ mecha_table <- read.csv('MechaCar_mpg.csv')
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_table)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_table))
+
+susp_table <- read.csv('Suspension_Coil.csv')
+
+total_summary <- summarize(susp_table, mean(PSI), median(PSI), var(PSI), sd(PSI))
+colnames(total_summary) <- c("Mean", "Median", "Variance", "SD")
+
+lot_summary = susp_table %>% group_by(Manufacturing_Lot)  %>%
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI),
+            .groups = 'drop')
